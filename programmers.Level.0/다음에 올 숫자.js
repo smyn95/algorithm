287ec3ca07bfda5244a8f4d4c8ef2170b749922c
smyn95 <tlsalduszz@gmail.com>
2022-12-03 12:26:54 +0900

    프로그래머스 다음에 올 숫자

---
+++ programmers.Level.0/다음에 올 숫자.js
@@ -0,0 +1,8 @@
+function solution(common) {
+  if (common[1] - common[0] === common[2] - common[1]) {
+    answer = common[common.length - 1] + (common[1] - common[0]);
+  } else {
+    answer = common[common.length - 1] * (common[1] / common[0]);
+  }
+  return answer;
+}
