8a362e8e95cdb96f0aa3f43a6d2333788156d6f2
smyn95 <tlsalduszz@gmail.com>
2022-12-10 20:47:14 +0900

    짝수의합 프로그래머스 가우스의 덧셈

--- programmers.Level.0/짝수의 합.js
+++ programmers.Level.0/짝수의 합.js
@@ -1,7 +1,12 @@
+// function solution(n) {
+//   let answer = 0;
+//   for (let i = 0; i <= n; i++) {
+//     if (i % 2 === 0) answer += i;
+//   }
+//   return answer;
+// }
+
 function solution(n) {
-  let answer = 0;
-  for (let i = 0; i <= n; i++) {
-    if (i % 2 === 0) answer += i;
-  }
-  return answer;
+  let num = ~~(n / 2);
+  return num * (num + 1);
 }
